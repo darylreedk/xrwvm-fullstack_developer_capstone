9476fb6935c9aa04e9d94c994338e23b7e005e6e
Welly789 <oliwelbourn1@gmail.com>
2024-04-30 19:44:39 -0400

    Final push on java linter

--- server/database/app.js
+++ server/database/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -34,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -63,7 +63,7 @@ app.get('/fetchDealers', async (req, res) => {
         const documents = await Dealerships.find();
         res.json(documents);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching documents' })
+        res.status(500).json({ error: 'Error fetching documents' });
     }
 });
 
@@ -71,10 +71,10 @@ app.get('/fetchDealers', async (req, res) => {
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
     try {
-        const documents = await Dealerships.find({state: req.params.state})
+        const documents = await Dealerships.find({state: req.params.state});
         res.json(documents);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching documents' })
+        res.status(500).json({ error: 'Error fetching documents' });
     }
 });
 
@@ -82,18 +82,18 @@ app.get('/fetchDealers/:state', async (req, res) => {
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
 try {
-    const documents = await Dealerships.find({id: req.params.id})
+    const documents = await Dealerships.find({id: req.params.id});
     res.json(documents);
 } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' })
+    res.status(500).json({ error: 'Error fetching documents' });
 }
 });
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0].id + 1;
 
   const review = new Reviews({
 		"id": new_id,

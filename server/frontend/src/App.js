31aaf0af1becf5744e9bf6ce48ff0eff61721b77
Daryl Reed Kritsch <83608143+darylreedk@users.noreply.github.com>
2024-11-16 14:44:58 -0800

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />}/>
       <Route path="/register" element={<RegisterContainer />}/>
+      <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>

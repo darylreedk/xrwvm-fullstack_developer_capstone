2190180fc6a5d8b83e9302d2f26000b8b8a4798f
Daryl Reed Kritsch <83608143+darylreedk@users.noreply.github.com>
2024-11-19 18:41:44 -0800

    restapis.py lint fixes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url', default = "http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
@@ -17,11 +17,11 @@ sentiment_analyzer_url = os.getenv(
 def get_request(endpoint, **kwargs):
     # Add code for get requests to back end
     params = ""
-    if(kwargs ):
+    if (kwargs):
         for key, value in kwargs.items():
-            params = params + key + "=" + value +"&"
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
@@ -34,7 +34,7 @@ def get_request(endpoint, **kwargs):
 
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     # Add code for retrieving sentiments
     try:
         # Call get method of requests library with URL and parameters
@@ -47,10 +47,10 @@ def analyze_review_sentiments(text):
 
 def post_review(data_dict):
     # Add code for posting review
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except Exceptio as e:
+    except Exception as e:
         print(f"Error: {e}")
